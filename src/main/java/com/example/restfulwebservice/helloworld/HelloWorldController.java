3a58b75ecdad1f07a32eec61dae489efba9d6e8a
Kong Sang Uk <qkqhfp19@naver.com>
2022-11-28 09:34:58 +0900

    패키지 이동

---
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice.helloworld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean (String.format("Hello World, %s" ,name));
+    }
+
+}
